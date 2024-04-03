58707d5413e1d3316bd642ba0f5e7eca52664d94
An1kun <boyb5147@gmail.com>
2024-04-04 03:04:03 +0500

    ImagesAdding

--- lib/domain/service/user_service_impl.dart
+++ lib/domain/service/user_service_impl.dart
@@ -4,14 +4,20 @@ import 'package:messager/domain/service/user_service.dart';
 class UserServiceImpl implements UserService {
   @override
   Future<User> readCurrentUser() async {
-    return const User(id: '1', name: 'User 1');
+    return const User(id: '1', name: 'Myself');
   }
 
   @override
   Future<List<User>> readUsers() async {
     return const <User>[
-      User(id: '2', name: 'User 2'),
-      User(id: '3', name: 'User 3'),
+      User(id: '2', name: 'Mother'),
+      User(id: '3', name: 'Papa'),
+      User(id: '4', name: 'Sanya'),
+      User(id: '5', name: 'Tonya'),
+      User(id: '6', name: 'Alikhan'),
+      User(id: '7', name: 'Maksat'),
+      User(id: '8', name: 'Zarina'),
+      User(id: '9', name: 'Aman'),
     ];
   }
 }
